ab243378333cbaf0a7538c101771a229e4ead56d
tutusvip <111453250+Tutusvip@users.noreply.github.com>
2022-10-12 14:29:13 -0300

    Add files via upload

---
+++ 2-att-sequencial.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL,"Portuguese_Brazil");
+	printf("Caderno de poesias é um belo lugar.\nTantas coisas lindas que eu gostaria de falar.\nEu falo em forma de versos para todos poderem escutar.\nAgora você já sabe por que os poetas passam os dias escrevendo em seus\ncadernos de poesias");
+	
+} 
