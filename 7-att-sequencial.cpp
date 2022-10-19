923fa79efa46f845b13ba6e0387479ef812f9ad4
tutusvip <111453250+Tutusvip@users.noreply.github.com>
2022-10-19 15:41:58 -0300

    Add files via upload

---
+++ 7-att-sequencial.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int f1, f2, prod;
+	
+	printf("Digite o primeiro fator: ");
+	scanf("%d", &f1);
+	printf("Digite o segundo fator: ");
+	scanf("%d", &f2);
+	
+	prod = f1 * f2;
+	
+	printf("PROD = %d", prod);
+}
